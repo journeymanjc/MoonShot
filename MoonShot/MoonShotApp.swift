0fc9c32e13ff238698fadf9f1d7327df022eca3d
Jae Cho <jjaecho@gmail.com>
2022-05-19 19:46:39 -0400

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Jae Cho on 5/19/22.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
