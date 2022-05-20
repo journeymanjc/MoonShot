43af3c34aa3ad6b3ff78084aaa7f10f750f51c6d
Jae Cho <jjaecho@gmail.com>
2022-05-19 20:38:37 -0400

    Day 39 : Using a JSON Decoder

--- MoonShot/ContentView.swift
+++ MoonShot/ContentView.swift
@@ -7,18 +7,38 @@
 
 import SwiftUI
 
+struct User: Codable{
+	let name : String
+	let address : Address
+}
+
+struct Address: Codable{
+	let street: String
+	let city: String
+}
+
+
+
 struct ContentView: View {
+	
+	
+	
     var body: some View {
-		 NavigationView{
-			 List(0..<100) { row in
-				 NavigationLink{
-					 Text("Detail \(row)")
-				 } label: {
-					 Text("Hello, World!")
-						 .padding()
-				 }
+		 Button("Decode JSON") {
+			 let input = """
+			{
+				"name": "Taylor Swift",
+				"address": {
+					"street": "555, Taylor Swift Avenue",
+					"city": "Nashville"
+				}
+			}
+"""
+			 let data = Data(input.utf8)
+			 let decoder = JSONDecoder()
+			 if let user = try? decoder.decode(User.self, from: data){
+				 print(user.address.street)
 			 }
-			 .navigationTitle("SwiftUI")
 		 }
     }
 }
