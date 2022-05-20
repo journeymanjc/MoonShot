51b4f28bc03684b68d9c3a8459f9e926d48046c8
Jae Cho <jjaecho@gmail.com>
2022-05-19 21:16:52 -0400

    Day 40 imported json and images for the project Also used extension on Bundle to take care of the decoding into a dictionary.

---
+++ MoonShot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  MoonShot
+//
+//  Created by Jae Cho on 5/19/22.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable{
+	let id: String
+	let name: String
+	let description: String
+}
--- MoonShot/ContentView.swift
+++ MoonShot/ContentView.swift
@@ -9,36 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
 	
-	let layout = [
-//		GridItem(.fixed(80)),
-//		GridItem(.fixed(80)),
-//		GridItem(.fixed(80))
-		GridItem(.adaptive(minimum: 80))
-	]
 	
+	let astronauts = Bundle.main.decode("astronauts.json")
 	
     var body: some View {
-		 ScrollView(.horizontal){
-			 LazyHGrid(rows: layout) {
-				 ForEach(0..<1000){
-					 Text("Item \($0)")
-				 }
-			 }
-		 }
+		 Text("\(astronauts.count)")
     }
 }
 
-struct CustomText: View{
-	let text: String
-	
-	var body: some View{
-		Text(text)
-	}
-	init(_ text: String){
-		print("Creating a new CustomText")
-		self.text = text
-	}
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
